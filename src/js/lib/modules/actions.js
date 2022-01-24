c7dd4a868782ffba328ac81d8955876487b70564
Andrey <expoder7@gmail.com>
2022-01-24 16:17:09 +0300

    upd 039 ex

--- src/js/lib/modules/actions.js
+++ src/js/lib/modules/actions.js
@@ -1,32 +1,69 @@
-import $ from '../core'
+import $ from '../core';
 
-$.prototype.on = function(eventName, callback) {
-    if(!eventName || !callback){
-        return this;
+
+$.prototype.html = function(content){
+    for(let i = 0 ; i < this.length;i++){
+        if(content){
+            this[i].innerHTML = content;
+        }else{
+            return this[i].innerHTML;
+        }
     }
-    for(let i = 0; i < this.length;i++){
-        this[i].addEventListener(eventName,callback);
+
+
+    return this;
+}
+
+$.prototype.eq = function(i){
+    
+    const swap = this[i];
+    const objectLength = Object.keys(this).length;
+
+    for(let i = 0; i < objectLength;i++){
+        delete this[i];
+    }
+
+    this[0] = swap;
+    this.length = 1;  
+
+    return this;
+}
+
+$.prototype.index = function(){
+    const parent = this[0].parentNode;
+    const childs = [...parent.children];
+
+    const findMyIndex = (item) =>{
+        return item == this[0];
+    }
+    return childs.findIndex(findMyIndex);  
+}
+
+
+$.prototype.index = function(selector){
+    let numberofItems = 0;
+    let counter = 0;
+
+    const copyObj = Object.assign({},this);
+
+    for(let i = 0 ; i < copyObj.length;i++){
+        const arr = copyObj[i].querySelectorAll(selector);
+        if(arr.length == 0){
+            continue;
+        }
+
+        for(let j = 0; j < arr.length;j++){
+            this[counter] = arr[j];
+            counter++;
+        }
+
+        numberofItems +=arr.length;
+    }
+
+    this.length = numberofItems;
+
+    for(;numberofItems < objectLength;numberofItems++){
+        delete this[numberofItems];
     }
     return this;
-};
-
-$.prototype.off = function(eventName, callback) {
-    if(!eventName || !callback){
-        return this;
-    }
-    for(let i = 0; i < this.length;i++){
-        this[i].removeEventListener(eventName,callback);
-    }
-    return this;
-};
-
-$.prototype.click = function(handler) {
-    for(let i = 0; i < this.length;i++){
-        if(handler){
-            this[i].addEventListener('click',handler);
-        } else {
-            this[i].click();
-        }       
-    }
-    return this;
-};
+}
--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,15 @@ import './lib/lib';
 
 //$('.active').toggle().toggle();
 $('button').on('click',function(){
-    $(this).hide().show().toggleClass('active');
+    //$(this).hide().show().toggleClass('active');
+    $('div').eq(0).toggleClass('active');
 });
+
+$('div').click(function(){
+    console.log($(this).index());
+});
+
+console.log($('div').eq(2).find('.more'));
+
+//console.log($('button').html("Hello"));
 
